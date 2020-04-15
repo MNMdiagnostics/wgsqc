1233432d05f2473b6acfb98c99b035c8ef3918a2
Rafal Stepien <rstepien095@gmail.com>
2020-04-15 13:41:35 +0200

    Merge all tables to one. Manually adding one record to database to check if database is creating properly.

--- database/base.py
+++ database/base.py
@@ -1,15 +1,37 @@
 #!/bin/python3
 
-# gene - meancov: one-to-many
-# gene - cov10: one-to-many
-# gene - cov20: one-to-many
-# gene - cov30: one-to-many
-# https://dbdiagram.io/d/5e96c35b39d18f5553fda003
-
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('postgresql://usr:pass@localhost:5432/sqlalchemy')
-Session = sessionmaker(bind=engine)
 Base = declarative_base()
+
+
+class Record(Base):
+    __tablename__ = 'record'
+
+    transcript_id = Column(String, primary_key=True)
+    sample_id = Column(String)
+    gene_id = Column(String)
+    mean_cov = Column(Float)
+    cov_10 = Column(Float)
+    cov_20 = Column(Float)
+    cov_30 = Column(Float)
+
+
+engine = create_engine("postgresql:///records.db", echo=True)
+Base.metadata.create_all(bind=engine)
+Session = sessionmaker(bind=engine)
+
+session = Session()
+record = Record()
+
+record.transcript_id = "NR_046018.2"
+record.sample_id = "MNM00001"
+record.gene_id = "DDX11L1"
+record.mean_cov = 53.27
+record.cov_10 = 96.85
+record.cov_20 = 92.62
+record.cov_30 = 86.26
+
+session.close()
