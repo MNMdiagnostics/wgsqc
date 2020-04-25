1971bd9c13531d55792d9f64147d2bbd76ccbd21
Rafal Stepien <rstepien095@gmail.com>
2020-04-25 22:30:11 +0200

    Add boxplots dynamicaly generated for mean_cov, x10, x20, x30

--- database/queries.py
+++ database/queries.py
@@ -2,6 +2,8 @@ import sys
 sys.path.append("..")
 from database.base import Session
 from collections import defaultdict
+import time
+import pprint
 
 
 def get_all_transcripts_names(table_name: "Class", type: "String"):
@@ -32,7 +34,11 @@ def get_transcripts_by_gene(table_name: "Class", type: "String"):
     """
     session = Session()
 
+    print("Querying...")
+    start = time.time()
     distincts = get_all_transcripts_names(table_name, type=type)
+    diff = time.time() - start
+    print(f"Query done, exec time {diff} seconds")
 
     dropdown_options = defaultdict(list)
     for obj in distincts:
@@ -60,3 +66,21 @@ def get_all_file_names(table_name: "Class", type: "String"):
     else:
         return [getattr(obj, type) for obj in files]
 
+
+def get_stats_for_plot(table_name, transcript, gene, stat):
+    """
+    :param table_name: Name of a table to get stats (eg. Record).
+    :param transcript: Transcript symbol to filter.
+    :param gene: Gene symbol to filter.
+    :param stat: Statistics to return (eg. mean_cov).
+    :return: List of wanted statistic values in all samples for given transcript and gene.
+    """
+    session = Session()
+
+    values = session.query(getattr(table_name, stat))\
+             .filter(table_name.transcript_id == transcript)\
+             .filter(table_name.gene_id == gene)\
+             .all()
+
+    session.close()
+    return [x[0] for x in values]
--- main/app.py
+++ main/app.py
@@ -1,15 +1,14 @@
 #!/bin/python3
-
 import sys
 import dash
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
+import plotly.graph_objs as go
 from dash.dependencies import Input, Output
 sys.path.append("..")
 from database.base import Record
-from database.queries import get_transcripts_by_gene
-import time
+from database.queries import get_transcripts_by_gene, get_stats_for_plot
 
 
 # --------------------------- STYLESHEETS AND APP SETUP ---------------------------
@@ -18,7 +17,6 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 # --------------------------- DATA LOAD ---------------------------
 dropdown_options = get_transcripts_by_gene(Record, type="object")
-
 
 # --------------------------- DROPDOWN MENUS SETTINGS ---------------------------
 genes_dropdown = dbc.Col(
@@ -39,14 +37,21 @@ dashboard = dbc.Navbar(
         dbc.Col(dbc.NavbarBrand(html.H3(name)), sm=3, md=2),
         genes_dropdown,
         transcripts_dropdown,
-        html.Div(id="display-selected-values")
     ],
     dark=True,
     color="#1a1a1a",
 )
 
+
+# --------------------------- DEFAULT BOXPLOT ---------------------------
+default_boxplots = go.Box(y=[])
+data = [default_boxplots]
+layout = go.Layout(title = "Mean coverage box plot")
+fig = go.Figure(data=data, layout=layout)
+boxplots = dcc.Graph(figure=fig, id='box-plot')
+
 # --------------------------- LAYOUT SETTINGS ---------------------------
-app.layout = html.Div([dashboard])
+app.layout = html.Div([dashboard, boxplots, html.Div(id="display-selected-values")])
 
 # --------------------------- DROPDOWNS CALLBACKS ---------------------------
 @app.callback(
@@ -64,14 +69,70 @@ def set_gene_value(available_options):
 
 
 @app.callback(
-    Output('display-selected-values', 'children'),
+    Output('box-plot', 'figure'),
     [Input('genes-dropdown', 'value'),
      Input('transcripts-dropdown', 'value')])
 def set_display_children(selected_gene, selected_transcript):
-    return u'{} {}'.format(
-        selected_gene, selected_transcript,
-    )
+    mean_cov_data = get_stats_for_plot(Record, selected_transcript, selected_gene, "mean_cov")
+    x10_cov_data = get_stats_for_plot(Record, selected_transcript, selected_gene, "cov_10")
+    x20_cov_data = get_stats_for_plot(Record, selected_transcript, selected_gene, "cov_20")
+    x30_cov_data = get_stats_for_plot(Record, selected_transcript, selected_gene, "cov_30")
+
+    mean_cov_boxplot = go.Box(
+        y=mean_cov_data,
+        name="Mean coverage",
+        jitter=0.3,
+        boxpoints='all',
+        marker=dict(
+            color='#7fdbff'),
+        line=dict(
+            color='#7fdbff'))
+
+    x10 = go.Box(
+        y=x10_cov_data,
+        name="Coverage X10",
+        jitter=0.3,
+        boxpoints='all',
+        marker=dict(
+            color='#7fdbff'),
+        line=dict(
+            color='#7fdbff'))
+
+    x20 = go.Box(
+        y=x20_cov_data,
+        name="Coverage X20",
+        jitter=0.3,
+        boxpoints='all',
+        marker=dict(
+            color='#7fdbff'),
+        line=dict(
+            color='#7fdbff'))
+
+    x30 = go.Box(
+        y=x30_cov_data,
+        name="Coverage X30",
+        jitter=0.3,
+        boxpoints='all',
+        marker=dict(
+            color='#7fdbff'),
+        line=dict(
+            color='#7fdbff'))
+
+    data = [mean_cov_boxplot, x10, x20, x30]
+
+    layout = go.Layout(title = "Coverage boxplots",
+                       height=800,
+                       paper_bgcolor='#010608',
+                       plot_bgcolor='#010608',
+                       font = {
+                           "size": 18,
+                           "color": '#7fdbff'
+                       }
+
+                       )
+    fig = go.Figure(data=data, layout=layout)
+    return fig
 
 
 if __name__ == "__main__":
-    app.run_server()
+    app.run_server(debug=True)
